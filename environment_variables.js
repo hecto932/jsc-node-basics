a5a70c38c920cb825ac5e926df64fec8da7fbfc9
Hector Flores <Hector_Flores@eogresources.com>
2020-05-20 21:17:43 -0700

    Monothread and environment variables

---
+++ environment_variables.js
@@ -0,0 +1,5 @@
+let name = process.env.NAME || 'Unnamed';
+let web = process.env.WEB || 'No site';
+
+console.log(`Hello ${name}!`);
+console.log(`My website is ${web}`);
---
+++ monothread.js
@@ -0,0 +1,5 @@
+console.log('hola mundo!');
+
+setInterval(function () {
+  console.log('Hola mundo! ');
+}, 1000);
